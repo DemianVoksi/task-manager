2ebaf92d365a3c134b6033967a8561311efdf4c9
Demian Voksi <demian.voksi@gmail.com>
2022-02-05 16:40:37 +0100

    css styling and db addition

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,8 @@
 const Footer = () => {
 
-  const madeBy = "Made by Demian Vokši, 2022"
-
   return ( 
     <div className='Footer'>
-      <h2> {madeBy} </h2>
+      <p>  </p>
     </div>
    );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 const Header = () => {
-  
-  const title = 'Task Manager'
 
   return ( 
     <div className='Header'>
-      <h2>{title}</h2>
+      <button className="btn" id="add-task" title="Add task">+</button>
     </div>
    );
 }
  
 export default Header;
+
+/*
+<button classname="btn" id="delete-task" title="Delete task"></button>
+<button className="btn" id="task-done" title="Task done"></button>
+<button className="btn" id="edit-task" title="Edit task"></button>
+*/
