db5c873a90dadcf38f7675ac393d06ae74ea6756
Demian Voksi <demian.voksi@gmail.com>
2022-03-01 01:10:27 +0100

    created and styled footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  
+  let footerText = "Created by Demian Vokši, 2022"
+  
+  return (
+    <div className='footer-container'>
+      <p className='footer-para'>{footerText}</p>
+    </div>
+  )
+}
+
+export default Footer
