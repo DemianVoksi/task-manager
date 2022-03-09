86d7b1d10f8e6483038e9dbb9095db479067ccbe
Demian Voksi <demian.voksi@gmail.com>
2022-03-09 23:56:17 +0100

    added firebase-config file, updated package.json

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from '@firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCU7EwQTFxFL6fNe4ab22oLj7JBGY9bwaw',
+	authDomain: 'task-manager-b5362.firebaseapp.com',
+	projectId: 'task-manager-b5362',
+	storageBucket: 'task-manager-b5362.appspot.com',
+	messagingSenderId: '49585478242',
+	appId: '1:49585478242:web:58c24ce614b5bd357ccf3b',
+	measurementId: 'G-9RVSEW287Z',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
