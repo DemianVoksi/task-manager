fc971c1afd5395bdad01273253756071bd56c56b
Demian Voksi <demian.voksi@gmail.com>
2022-03-10 22:31:07 +0100

    imported database to MainForm, edited fetchTask and fetchTasks

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,126 +1,158 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ListTasks from './ListTasks';
+import { db } from '../firebase-config';
+import {
+	collection,
+	query,
+	orderBy,
+	getDocs,
+	getDoc,
+	doc,
+} from 'firebase/firestore';
 
 function MainForm() {
+	const [taskName, setTaskName] = useState('');
+	const [taskTime, setTaskTime] = useState('');
+	const [taskDone, setTaskDone] = useState(false);
+	const [allTasks, setAllTasks] = useState([]);
+	const tasksCollectionRef = collection(db, 'tasks');
+	const tasksOrdered = query(tasksCollectionRef, orderBy('taskTimestamp'));
 
-  const [taskName, setTaskName] = useState('');
-  const [taskTime, setTaskTime] = useState('');
-  const [taskDone, setTaskDone] = useState(false);
-  const [allTasks, setAllTasks] = useState([]);
+	/* ***FETCH ALL TASKS*** */
 
+	// const fetchTasks = async () => {
+	// 	let tasksFromServer = await fetch('http://localhost:8000/tasks');
+	// 	let data = await tasksFromServer.json();
+	// 	return data;
+	// };
 
-  const fetchTask = async(id) => {
-    const taskFromServer = await fetch(`http://localhost:8000/tasks/${id}`);
-    const data = await taskFromServer.json();
-    return data;
-  }
+	const fetchTasks = async () => {
+		const data = await getDocs(tasksOrdered);
+		setAllTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+	};
 
+	/* ***FETCH SINGLE TASK*** */
 
-  const fetchTasks = async() => {    
-    let tasksFromServer = await fetch('http://localhost:8000/tasks');
-    let data = await tasksFromServer.json();
-    return data;
-  }
+	// const fetchTask = async (id) => {
+	// 	const taskFromServer = await fetch(`http://localhost:8000/tasks/${id}`);
+	// 	const data = await taskFromServer.json();
+	// 	return data;
+	// };
 
+	const fetchTask = async (id) => {
+		const docRef = doc(db, 'tasks', id);
+		const docSnap = await getDoc(docRef);
+		return docSnap;
+	};
 
-  async function submitToServer(e) {    
-    e.preventDefault()
+	/* ***SUBMIT ALL TASKS*** */
 
-    let taskToSubmit = { taskName, taskTime, taskDone };
-    let parameters = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      mode: 'cors',
-      body: JSON.stringify(taskToSubmit)
-    }
+	async function submitToServer(e) {
+		e.preventDefault();
 
-    const submitting = await fetch('http://localhost:8000/tasks', parameters);
-    const data = await submitting.json()
-    setAllTasks([...allTasks, data])
-    setTaskName('')
-    setTaskTime('')
-  }
+		let taskToSubmit = { taskName, taskTime, taskDone };
+		let parameters = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			mode: 'cors',
+			body: JSON.stringify(taskToSubmit),
+		};
 
+		const submitting = await fetch('http://localhost:8000/tasks', parameters);
+		const data = await submitting.json();
+		setAllTasks([...allTasks, data]);
+		setTaskName('');
+		setTaskTime('');
+	}
 
-  useEffect(() => {
-    const fromServer = async () => {
-      const tasksFromServer = await fetchTasks()
-      setAllTasks(tasksFromServer)
-    }
+	/* ***USE EFFECT*** */
 
-    fromServer()
-  }, [])
+	useEffect(() => {
+		const fromServer = async () => {
+			const tasksFromServer = await fetchTasks();
+			setAllTasks(tasksFromServer);
+		};
 
+		fromServer();
+	}, []);
 
-  const toggleDone = async(id) => {
-    const toChange = await fetchTask(id);
-    const updated = {...toChange, taskDone: !toChange.taskDone}
+	/* ***TOGGLE DONE*** */
 
-    const resolve = await fetch(`http://localhost:8000/tasks/${id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify(updated)
-    });
+	const toggleDone = async (id) => {
+		const toChange = await fetchTask(id);
+		const updated = { ...toChange, taskDone: !toChange.taskDone };
 
-    let data = await resolve.json()
-    
-    setAllTasks(
-      allTasks.map((item) => (
-        item.id === id ? {...item, taskDone: data.taskDone} : item
-      ))
-    )
-  }
+		const resolve = await fetch(`http://localhost:8000/tasks/${id}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(updated),
+		});
 
+		let data = await resolve.json();
 
-  const onDelete = async(id) => {
-    const resolve = await fetch(`http://localhost:8000/tasks/${id}`, {
-      method: 'DELETE'
-    })
+		setAllTasks(
+			allTasks.map((item) =>
+				item.id === id ? { ...item, taskDone: data.taskDone } : item
+			)
+		);
+	};
 
-    resolve.status === 200 
-    ? setAllTasks(allTasks.filter((task) => task.id !== id))
-    : alert("Error")
-  }
+	/* ***ON DELETE*** */
 
+	const onDelete = async (id) => {
+		const resolve = await fetch(`http://localhost:8000/tasks/${id}`, {
+			method: 'DELETE',
+		});
 
-  return (
-    <div className='Form-wrapper'>
-      <form 
-      className='form' 
-      name='form' 
-      onSubmit={submitToServer}>
-        <div className='enter-task-div'>
-          <input
-          type='text'
-          className='enter-task-input'
-          name='enterTask'
-          value={taskName}
-          placeholder='Enter task'
-          onChange={(e) => {setTaskName(e.target.value)}}
-          required />
-        </div>
-        <div className='enter-time-div'>
-          <input
-          type='text'
-          className='enter-time-input' 
-          name='enterTime'
-          placeholder='Enter time'
-          value={taskTime}
-          onChange={(e) => {setTaskTime(e.target.value)}}
-          required />
-        </div>
-        <div className='btn-div'>
-          <button 
-          type='submit'
-          className='btn'>Remind me</button>
-        </div>
-      </form>
-      {allTasks && <ListTasks 
-      allTasks={allTasks} 
-      toggleDone={toggleDone}
-      onDelete={onDelete} />}
-    </div>
-  )
+		resolve.status === 200
+			? setAllTasks(allTasks.filter((task) => task.id !== id))
+			: alert('Error');
+	};
+
+	return (
+		<div className='Form-wrapper'>
+			<form className='form' name='form' onSubmit={submitToServer}>
+				<div className='enter-task-div'>
+					<input
+						type='text'
+						className='enter-task-input'
+						name='enterTask'
+						value={taskName}
+						placeholder='Enter task'
+						onChange={(e) => {
+							setTaskName(e.target.value);
+						}}
+						required
+					/>
+				</div>
+				<div className='enter-time-div'>
+					<input
+						type='text'
+						className='enter-time-input'
+						name='enterTime'
+						placeholder='Enter time'
+						value={taskTime}
+						onChange={(e) => {
+							setTaskTime(e.target.value);
+						}}
+						required
+					/>
+				</div>
+				<div className='btn-div'>
+					<button type='submit' className='btn'>
+						Remind me
+					</button>
+				</div>
+			</form>
+			{allTasks && (
+				<ListTasks
+					allTasks={allTasks}
+					toggleDone={toggleDone}
+					onDelete={onDelete}
+				/>
+			)}
+		</div>
+	);
 }
 
-export default MainForm
+export default MainForm;
