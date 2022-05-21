b442aa818db3cac2cb371b590e89d7ce4c58f527
Demian Voksi <demian.voksi@gmail.com>
2022-05-21 17:23:32 +0200

    all done except for a type error

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import {
 	signOut
 } from 'firebase/auth';
 import Login from './components/Login';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import MainForm from './components/MainForm';
 import './index.css';
 import Home from './components/Home';
-import { AuthProvider } from './utils/auth';
+import { AuthProvider, useAuth } from './utils/auth';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import RequireAuth from './utils/RequireAuth';
 
 /*
 1) DONE Header, MainForm i Footer moraju ići u novi component Home,
@@ -20,9 +19,9 @@ a njihov div unutar kojeg se nalaze mora imati className='logged-in'
 2) DONE Login i Home componentsi idu u routes, Home kao '/', 
 Login kao '/login'
 3) DONE Napraviti utils folder i u njega staviti auth.js i RequireAuth.js
-4) U auth.js dodati state userLoggedIn koje se mijenja kada se login i logout 
+4) DONE U auth.js dodati state userLoggedIn koje se mijenja kada se login i logout 
 funkcije iz auth.js aktiviraju.
-5) Dodati te funkcije u login i logout funkcije u App.js
+5) DONE Dodati te funkcije u login i logout funkcije u App.js
 */
 
 function App() {
@@ -31,6 +30,8 @@ function App() {
 	const [loginEmail, setLoginEmail] = useState('');
 	const [loginPassword, setLoginPassword] = useState('');
 	const [user, setUser] = useState({});
+	const uAuth = useAuth();
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		onAuthStateChanged(auth, (currentUser) => {
@@ -46,6 +47,8 @@ function App() {
 				registerEmail,
 				registerPassword
 			);
+			await uAuth.loginAuth();
+			navigate('/');
 			console.log(user);
 		} catch (error) {
 			console.log(error);
@@ -54,12 +57,15 @@ function App() {
 
 	const login = async (e) => {
 		e.preventDefault();
+
 		try {
 			const user = await signInWithEmailAndPassword(
 				auth,
 				loginEmail,
 				loginPassword
 			);
+			await uAuth.loginAuth();
+			navigate('/');
 			console.log(user);
 		} catch (error) {
 			console.log(error);
@@ -68,32 +74,40 @@ function App() {
 
 	const logout = async () => {
 		await signOut(auth);
+		await uAuth.logoutAuth();
+		navigate('/login');
 	};
 
 	return (
-		<div className='App'>
-			{/* <Home logout={logout} user={user} /> */}
-			{user?.email ? (
-				<div className='logged-in'>
-					<Header logout={logout} />
-					<MainForm user={user} />
-					<Footer />
-				</div>
-			) : (
-				<Login
-					registerEmail={registerEmail}
-					registerPassword={registerPassword}
-					loginEmail={loginEmail}
-					loginPassword={loginPassword}
-					setRegisterEmail={setRegisterEmail}
-					setRegisterPassword={setRegisterPassword}
-					setLoginEmail={setLoginEmail}
-					setLoginPassword={setLoginPassword}
-					register={register}
-					login={login}
-				/>
-			)}
-		</div>
+		<AuthProvider>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<RequireAuth>
+							<Home logout={logout} user={user} />
+						</RequireAuth>
+					}
+				></Route>
+				<Route
+					path='/login'
+					element={
+						<Login
+							registerEmail={registerEmail}
+							registerPassword={registerPassword}
+							loginEmail={loginEmail}
+							loginPassword={loginPassword}
+							setRegisterEmail={setRegisterEmail}
+							setRegisterPassword={setRegisterPassword}
+							setLoginEmail={setLoginEmail}
+							setLoginPassword={setLoginPassword}
+							register={register}
+							login={login}
+						/>
+					}
+				></Route>
+			</Routes>
+		</AuthProvider>
 	);
 }
 
